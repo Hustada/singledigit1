b27b76c3c2492043d14d7a45bab2abf27e14bb6f
Mark <hustada80@gmail.com>
2014-11-17 23:49:54 -0600

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.paginate(page: params[:page], per_page: 1)
+    ##@posts_by_month = Post.find(:all, :order => 'created_at DESC').group_by { |post| post.created_at.strftime("%B %Y") }
+  end
+
+
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+
+  def create
+    @post = Post.new(post_params)
+  if @post.save
+    flash[:notice] = "Your fucking post has been created BITCH!"
+    redirect_to @post
+  end
+end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :image)
+  end
+
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Thanks for signing up."
+      redirect_to root_url 
+    else
+      render "new"
+    end
+  end
+
+   def new
+    @user = User.new   
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
